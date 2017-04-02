da797544a841ff0dfb0d4eed92093972f6a70498
Maxn <nash2665@gmail.com>
2017-04-02 18:38:20 +0800

    Hw04 -1

--- script/script.js
+++ script/script.js
@@ -18,6 +18,7 @@ d3.csv("data/invoice.csv", mid, function (dataSet) {
     data = dataSet;
     binder(dataSet);
     render(dataSet);
+    btnList(unique(dataSet));
 });
 
 //d3讀取的資料皆為字串
@@ -39,8 +40,16 @@ function svg() {
         .attr({
             "width": "100%",
             "height": "100%",
-            "fill": "none"
+            "fill": "white"
         });
+
+    svg.append("g")
+        .classed("axis", true)
+        .attr("id", "axisX");
+
+    svg.append("g")
+        .classed("axis", true)
+        .attr("id", "axisY");
 }
 
 //綁定元素
@@ -110,6 +119,8 @@ function render(data) {
         })
 
     d3.selectAll("circle")
+        .transition()
+        .duration(1000)
         .attr({
             "cx": function (d) {
                 return xScale(new Date(d.date));
@@ -125,15 +136,96 @@ function render(data) {
             }
         });
 
-    d3.select("svg")
-        .append("g")
-        .attr("class", "axis")
+    d3.selectAll("circle")
+        .on("mouseover", function (d) {
+            var posiX = d3.select(this).attr("cx");
+            var posiY = d3.select(this).attr("cy");
+            //            console.log(posiX);
+            var tooltip = d3.select("#tooltip")
+                .style({
+                    "left": (+posiX + 20) + "px",
+                    "top": (+posiY + 20) + "px"
+                });
+
+            tooltip.select("#city").text(d.city);
+            tooltip.select("#industry").text(d.industry);
+
+            tooltip.classed("hidden", false);
+        })
+        .on("mouseout", function (d) {
+            d3.select("#tooltip").classed("hidden", true);
+        });
+
+
+
+    d3.select("svg>#axisX")
         .attr("transform", "translate(" + 0 + "," + (height - padding.bottom + 10) + ")")
         .call(axisX);
 
-    d3.select("svg")
-        .append("g")
-        .attr("class", "axis")
+    d3.select("svg>#axisY")
         .attr("transform", "translate(" + (padding.left - 10) + "," + 0 + ")")
         .call(axisY);
+}
+
+//列出button
+function btnList(data) {
+
+    var narray = data.map(function (d) {
+        return d.industry;
+    });
+
+    var fnarry = unique(narray);
+
+    var farry = fnarry.filter(function (d) {
+        return d != "";
+    });
+    var selection = d3.select("body")
+        .append("select")
+        .selectAll("option")
+        .data(farry);
+
+    selection.enter()
+        .append("option");
+
+    selection.exit()
+        .remove();
+
+    d3.selectAll("option")
+        .attr({
+            "value": function (d) {
+                return d;
+            }
+        })
+        .text(function (d) {
+            return d;
+        });
+
+    d3.selectAll("select")
+        .on("change", function (d) {
+            var value = d3.select("select").property("value");
+            //            console.log(value);
+            update(value);
+        });
+
+    function update(da) {
+        var fdata = data.filter(function (d) {
+            return d.industry === da;
+        });
+
+        binder(fdata);
+        render(fdata);
+
+    }
+}
+
+function unique(array) {
+    var narry = [];
+
+    for (var n = 0; n < array.length; n++) {
+        if (narry.indexOf(array[n]) === -1) {
+            narry.push(array[n]);
+        }
+    }
+    //    console.log(narry);
+    return narry;
 }
